0c99e8af93e573cdf99397aa7fb0110a4513fb6c
ViktoriyaGalkina <raketa3390@inbox.ru>
2023-03-18 00:03:02 +0300

    Write a program

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -8,7 +8,7 @@
 // ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
- 
+
 
 int ArrayLengthCheck(string[] array)
 {
